28639d8493de571c3d4c952ff0d57a561dccbd25
memin09 <benjamincastaneda783@gmial.com>
2025-06-13 12:56:08 -0600

    prueba config

--- services/eureka/src/test/java/com/example/eureka/EurekaApplicationTests.java
+++ services/eureka/src/test/java/com/example/eureka/EurekaApplicationTests.java
@@ -1,10 +1,10 @@
-package com.microservicos.discovery;
+package com.example.eureka;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DiscoveryApplicationTests {
+class EurekaApplicationTests {
 
 	@Test
 	void contextLoads() {
